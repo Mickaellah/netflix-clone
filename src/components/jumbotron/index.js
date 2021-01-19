03cda70680a27a16ba8302d7331694e3ef2c6605
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2021-01-19 11:55:24 +0300

    Worked with the jumbotron part for the netflix clone project.

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {Item, Inner, Container, Title, SubTitle, Image, Pane} from './styles/jumbotron';
 
 export default function Jumbotron({ children, direction = 'row', ...restProps }) {
     return (
@@ -8,4 +9,22 @@ export default function Jumbotron({ children, direction = 'row', ...restProps })
     )
 }
 
-Jumbotron.Container = function JumbotonContainer() {}
+Jumbotron.Container = function JumbotonContainer({children, ...restProps}) {
+    return <Container {...restProps}>{children}</Container>
+}
+
+Jumbotron.Title = function JumbotonTitle({children, ...restProps}) {
+    return <Title {...restProps}>{children}</Title>
+}
+
+Jumbotron.SubTitle = function JumbotonSubTitle({children, ...restProps}) {
+    return <SubTitle {...restProps}>{children}</SubTitle>
+}
+
+Jumbotron.Image = function JumbotonImage({children, ...restProps}) {
+    return <Image {...restProps} />
+}
+
+Jumbotron.Pane = function JumbotonPane({children, ...restProps}) {
+    return <Pane {...restProps}>{children}</Pane>
+}
